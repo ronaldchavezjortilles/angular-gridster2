84c6675442bf444c8f6f28f702056e663efd1352
ronaldchavezjortilles <ronald.chavez@jortilles.com>
2024-11-22 12:41:54 +0100

    Angular 14 - Versión básica de angular-gridster2 con la versión 14.x

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 // Modulos
 import { AppRoutingModule } from './app-routing.module';
+import { GridsterModule } from 'angular-gridster2';
+import { MatIconModule } from '@angular/material/icon';
+import { MarkdownModule } from 'ngx-markdown';
+
 
 import { AppComponent } from './app.component';
 import { InicioComponent } from './pages/inicio/inicio.component';
 import { NosotrosComponent } from './pages/nosotros/nosotros.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,7 +21,11 @@ import { NosotrosComponent } from './pages/nosotros/nosotros.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GridsterModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MarkdownModule.forRoot(),  // Asegúrate de agregar MarkdownModule aquí
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -1,15 +1,105 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  ViewEncapsulation
+} from '@angular/core';
+
+import {
+  CompactType,
+  GridsterConfig,
+  GridsterItem,
+  GridsterItemComponent,
+  GridsterPush,
+  GridType
+} from 'angular-gridster2';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
-  styleUrls: ['./inicio.component.css']
+  styleUrls: ['./inicio.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None
 })
 export class InicioComponent implements OnInit {
+  options: GridsterConfig;
+  dashboard: GridsterItem[];
+  itemToPush!: GridsterItemComponent;
 
-  constructor() { }
+  constructor() {
+    this.options = {
+      gridType: GridType.Fit,
+      compactType: CompactType.None,
+      pushItems: true,
+      draggable: {
+        enabled: true
+      },
+      resizable: {
+        enabled: true
+      }
+    };
+  
+    this.dashboard = [
+      { cols: 2, rows: 1, y: 0, x: 0, initCallback: this.initItem.bind(this) },
+      { cols: 2, rows: 2, y: 0, x: 2 },
+      { cols: 1, rows: 1, y: 0, x: 4 },
+      { cols: 3, rows: 2, y: 1, x: 4 },
+      { cols: 1, rows: 1, y: 4, x: 5 },
+      { cols: 1, rows: 1, y: 2, x: 1 },
+      { cols: 2, rows: 2, y: 5, x: 5 },
+      { cols: 2, rows: 2, y: 3, x: 2 },
+      { cols: 2, rows: 1, y: 2, x: 2 },
+      { cols: 1, rows: 1, y: 3, x: 4 },
+      { cols: 1, rows: 1, y: 0, x: 6 }
+    ];
+  }
 
   ngOnInit(): void {
   }
 
+  changedOptions(): void {
+    if (this.options.api && this.options.api.optionsChanged) {
+      this.options.api.optionsChanged();
+    }
+  }
+
+  removeItem($event: MouseEvent | TouchEvent, item: any): void {
+    $event.preventDefault();
+    $event.stopPropagation();
+    this.dashboard.splice(this.dashboard.indexOf(item), 1);
+  }
+
+  addItem(): void {
+    this.dashboard.push({ x: 0, y: 0, cols: 1, rows: 1 });
+  }
+
+  initItem(item: GridsterItem, itemComponent: any): void {
+    this.itemToPush = itemComponent;
+  }
+
+  pushItem(): void {
+    const push = new GridsterPush(this.itemToPush); // init the service
+    this.itemToPush.$item.rows += 4; // move/resize your item
+    if (push.pushItems(push.fromNorth)) {
+      // push items from a direction
+      push.checkPushBack(); // check for items can restore to original position
+      push.setPushedItems(); // save the items pushed
+      this.itemToPush.setSize();
+      this.itemToPush.checkItemChanges(
+        this.itemToPush.$item,
+        this.itemToPush.item
+      );
+    } else {
+      this.itemToPush.$item.rows -= 4;
+      push.restoreItems(); // restore to initial state the pushed items
+    }
+    push.destroy(); // destroy push instance
+    // similar for GridsterPushResize and GridsterSwap
+  }
+
+  getItemComponent(): void {
+    if (this.options.api && this.options.api.getItemComponent) {
+      console.log(this.options.api.getItemComponent(this.dashboard[0]));
+    }
+  }
 }
