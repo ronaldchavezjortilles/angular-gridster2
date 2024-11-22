9f4cd01fa93dc3af6114635a74439b4dd6ee5d1c
ronaldchavezjortilles <ronald.chavez@jortilles.com>
2024-11-22 10:17:28 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Modulos
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { NosotrosComponent } from './pages/nosotros/nosotros.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    NosotrosComponent
   ],
   imports: [
     BrowserModule,
