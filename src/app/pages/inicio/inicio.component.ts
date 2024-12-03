0a7adc21319d4435b8cc9edf624754c0ebf241c9
ronaldchavezjortilles <ronald.chavez@jortilles.com>
2024-12-03 14:07:43 +0100

    Gridster funcional

--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -7,6 +7,7 @@ import {
 
 import {
   CompactType,
+  DisplayGrid,
   GridsterConfig,
   GridsterItem,
   GridsterItemComponent,
@@ -26,19 +27,32 @@ export class InicioComponent implements OnInit {
   dashboard: GridsterItem[];
   itemToPush!: GridsterItemComponent;
 
+  public height: number = 0;
+
+
   constructor() {
     this.options = {
-      gridType: GridType.Fit,
+      gridType: GridType.VerticalFixed,
       compactType: CompactType.None,
+      displayGrid: DisplayGrid.OnDragAndResize,
       pushItems: true,
+      // disableScrollVertical: true, // Desactiva scroll interno
       draggable: {
         enabled: true
       },
       resizable: {
         enabled: true
-      }
+      },
+      maxCols: 7,
+      minCols: 7,
+      minRows: 10,
+      maxRows: 300,
+      disableScrollHorizontal: true, // Desactiva scroll horizontal si es necesario
+      disableScrollVertical: true, // Desactiva scroll horizontal si es necesario
+      itemChangeCallback: (item: GridsterItem) => this.onItemChange(item),
+      itemResizeCallback: (item: GridsterItem) => this.onItemChange(item)
     };
-  
+
     this.dashboard = [
       { cols: 2, rows: 1, y: 0, x: 0, initCallback: this.initItem.bind(this) },
       { cols: 2, rows: 2, y: 0, x: 2 },
@@ -54,8 +68,10 @@ export class InicioComponent implements OnInit {
     ];
   }
 
+
   ngOnInit(): void {
   }
+
 
   changedOptions(): void {
     if (this.options.api && this.options.api.optionsChanged) {
@@ -67,6 +83,7 @@ export class InicioComponent implements OnInit {
     $event.preventDefault();
     $event.stopPropagation();
     this.dashboard.splice(this.dashboard.indexOf(item), 1);
+    this.onItemChange(item);
   }
 
   addItem(): void {
@@ -99,7 +116,45 @@ export class InicioComponent implements OnInit {
 
   getItemComponent(): void {
     if (this.options.api && this.options.api.getItemComponent) {
+      console.log('this.dashboard: ',this.dashboard)
       console.log(this.options.api.getItemComponent(this.dashboard[0]));
     }
+  }
+
+  onItemChange(item: GridsterItem): void {
+    console.log('Cambio en el ítem:', item);
+    console.log('Todos los valores:', this.dashboard);
+
+    const bottomMostItem = this.getBottomMostItem()
+    let altura = (bottomMostItem!.y + bottomMostItem!.cols + 3) * 250;
+    if(altura<5){
+      this.height = 4*250;
+    } else {
+      this.height = altura;
+    }
+
+    // this.height = 4 * 250
+
+    // this.getBottomMostItem(); // Actualizamos el ítem más bajo cada vez que algo cambia
+  }
+
+    // Función para detectar el ítem más bajo
+  getBottomMostItem(): GridsterItem | undefined {
+    let bottomMostItem: GridsterItem | undefined;
+    let maxBottom = -1; // Inicializamos con un valor bajo
+
+    for (let item of this.dashboard) {
+      // Calculamos la posición final en Y (bottom) del ítem
+      const bottom = item.y + item.rows;
+
+      // Si el ítem actual es más bajo, lo actualizamos
+      if (bottom > maxBottom) {
+        maxBottom = bottom;
+        bottomMostItem = item;
+      }
+    }
+
+    console.log('Ítem más bajo:', bottomMostItem);
+    return bottomMostItem;
   }
 }
